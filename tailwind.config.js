9c6233598bb62e345296ef7656da3f414d10b4aa
xasanovv <xasanovibrohim555@gmail.com>
2023-04-20 06:44:01 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors : {
+      'soft-orange': 'hsl(35, 77%, 62%)',
+      'soft-red': 'hsl(5, 85%, 63%)',
+      'off-white': 'hsl(36, 100%, 99%)',
+      'grayish-blue': 'hsl(233, 8%, 79%)',
+      'dark-grayish-blue': 'hsl(236, 13%, 42%)',
+      'very-dark-blue': 'hsl(240, 100%, 5%)'
+    },
+    fontFamily: {
+      inter:['Inter','sans-serif']
+    }
+  },
+  plugins: [],
+};
+
